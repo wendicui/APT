14834084efbb7fa5eef4d2dadd9bd00b56a8f4eb
Yaya Du <Yayadu@Yayas-MacBook-Pro.local>
2017-10-04 21:05:46 -0400

    fixed space issue

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -2,24 +2,24 @@ $(document).ready(function(){
 
 
 
-    function firstsearch() {
-        window.location.href = "search.html";
-    }
+    // function firstsearch() {
+    //     window.location.href = "search.html";
+    // }
 
-    $("#firstsearch").click(firstsearch);
+    // $("#firstsearch").click(firstsearch);
 
  // create function for click event   
     function drawData(){
-        console.log("working")
+        
         var address1 = "468%20SEQUOIA%20DR"
         var address2 = "SMYRNA%2C%20DE"
         var radius = "20"
         var min 
         var max
+        
+        var url = `https://search.onboard-apis.com/propertyapi/v1.0.0/assessment/snapshot?address1=${address1}&address2=${address2}&radius=${radius}`
 
-        var url = `https://search.onboard-apis.com/propertyapi/v1.0.0/
-        assessment/snapshot?address1=${address1}&address2=${address2}
-        &radius=${radius}`
+        console.log(url)
 
         $.ajax({
             url:url,
@@ -34,6 +34,6 @@ $(document).ready(function(){
 
 
 
-    $("button").on("click", drawData)
+$("button").on("click", drawData)
 
 });
