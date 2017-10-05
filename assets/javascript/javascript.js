e741c2e4cd6bd6e08a420ef76670dd05e63b3c76
iamarirosenthal <iamarirosenthal@outlook.com>
2017-10-04 20:47:12 -0400

    changes again

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -4,9 +4,17 @@ $(document).ready(function(){
 
     function firstsearch() {
         window.location.href = "search.html";
-    }
+    } 
+  mapboxgl.accessToken = 'pk.eyJ1IjoiaWFtYXJpcm9zZW50aGFsIiwiYSI6ImNqOGRsc204bzBwbnAyd2xibnpqZG4wYmMifQ.rtRd33gsmY5DGiQkkJoj9g';
+  var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v10'
+  });
+
+
 
     $("#firstsearch").click(firstsearch);
 
 
 });
+
